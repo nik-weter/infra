3fa68dfb07574864805c9622a53064e47b85e60b
nik-weter <35607485+nik-weter@users.noreply.github.com>
2019-07-19 14:17:13 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 apt-get update&&apt-get install git
-mkdir infra&&cd infra
+#mkdir infra&&cd infra
 git clone https://github.com/nik-weter/infra.git
+cd infra
 chmod +x *.sh
 bash install_ruby.sh
 bash install_mongodb.sh
