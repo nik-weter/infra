62158c6117f963c797efe18a8a2bf008855f723e
Савельев Николай <nik-weter@yandex.ru>
2019-07-24 16:29:44 +0700

    Add sudo -u appuser in scripts

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt-get update&&sudo apt-get install git
 #mkdir infra&&cd infra
-git clone https://github.com/nik-weter/infra.git
-cd infra
+sudo -u appuser git clone https://github.com/nik-weter/infra.git /home/appuser/infra
+cd /home/appuser/infra
 chmod +x *.sh
-bash install_ruby.sh
+sudo -u appuser bash install_ruby.sh
 bash install_mongodb.sh
-bash deploy.sh
+sudo -u appuser bash deploy.sh
